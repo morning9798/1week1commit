9391c577f681aac2f99b1ea4b582566da356a630
morning9798 <modelia3@naver.com>
2022-04-01 20:59:05 +0900

    사칙연산만들기 예제 (비쥬얼베이직)

---
+++ 사칙연산프로그램만들기/사칙연산프로그램만들기/Form2.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form2
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim iSutja As Integer
+        Dim dSum As Double
+
+        iSutja = TextBox1.Text
+
+        TextBox2.Text = ""
+
+        For i = 1 To iSutja Step 1
+            dSum = dSum + i
+            TextBox2.Text = TextBox2.Text & i & " + "
+        Next
+        TextBox2.Text = Microsoft.VisualBasic.Left(TextBox2.Text, Len(TextBox2.Text) - 2)
+        TextBox2.Text = TextBox2.Text & " = " & dSum
+    End Sub
+End Class
---
+++ 사칙연산프로그램만들기/사칙연산프로그램만들기/Form4.vb
@@ -0,0 +1,34 @@
+﻿Public Class Form4
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim iSutja As Integer
+        Dim dNum_Odd, dNum_Even As Double
+
+
+        iSutja = TextBox1.Text
+
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+
+        For i = 1 To iSutja Step 1
+            If i Mod 2 = 0 Then '짝수
+                dNum_Even = iSutja + i
+                If i = iSutja Or i = iSutja - 1 Then
+                    TextBox3.Text = TextBox3.Text & i & " = "
+                Else
+                    TextBox3.Text = TextBox3.Text & i & " + "
+                End If
+            Else
+                dNum_Odd = iSutja + i
+                If i = iSutja Or i = iSutja - 1 Then
+                    TextBox2.Text = TextBox2.Text & i & " = "
+                Else
+                    TextBox2.Text = TextBox2.Text & i & " + "
+
+                End If
+            End If
+        Next
+        TextBox2.Text = TextBox2.Text & dNum_Odd
+        TextBox3.Text = TextBox3.Text & dNum_Even
+
+    End Sub
+End Class
