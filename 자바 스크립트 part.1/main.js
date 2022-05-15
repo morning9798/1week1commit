4bff4f0500f043cb7de562e2d897cae0f78e518e
morning9798 <modelia3@naver.com>
2022-05-15 18:28:03 +0900

    자바 스크립트 개념

---
+++ 자바 스크립트 part.1/main.js
@@ -0,0 +1,9 @@
+// 선언을 할때 var,let
+var heading = document.querySelector('#heading');
+heading.onclick = function(){
+    heading.style.color = "red";
+}
+let fruits=['Apple','Banana','Cherry'];
+// 배열로 여러개의 값을 적을 때 사용한다.
+// 배열과 객체데이터
+console.log(fruits[2]);
