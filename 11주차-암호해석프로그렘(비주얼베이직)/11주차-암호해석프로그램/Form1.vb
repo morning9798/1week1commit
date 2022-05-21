c3d5664ae0e62af9a9d2a36ef8e48cc31a2b1b8b
morning9798 <modelia3@naver.com>
2022-05-22 03:35:16 +0900

    암호해석프로그램(비주얼베이직)

---
+++ 11주차-암호해석프로그렘(비주얼베이직)/11주차-암호해석프로그램/Form1.vb
@@ -0,0 +1,104 @@
+﻿Public Class 배열로
+    Dim sAlphabet_Small() As String = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
+    Dim sAlphabet_Big() As String = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.Text = "" Then
+            MessageBox.Show("평문을 영어 소문자로 입력하세요", "경고 1")
+            TextBox1.Focus()
+            Exit Sub
+        End If
+
+        Dim iMunjang_Len As Integer
+        iMunjang_Len = Len(TextBox1.Text)
+        Dim sMunjang(iMunjang_Len) As String
+
+        For i = 0 To iMunjang_Len - 1
+            sMunjang(i) = Mid(TextBox1.Text, i + 1, 1) ' 문장을 한글자씩 잘라서 배열에 저장
+        Next
+
+        'TextBox2.Text = ""
+        'For i = 0 To iMunjang_Len - 1
+        '   TextBox2.Text = TextBox2.Text & sMunjang(i) ' 저장한 배열을 출력
+        'Next
+
+        Dim iCount As Integer = 0
+        For i = 0 To iMunjang_Len - 1
+            For j = 0 To 25
+                If sMunjang(i) = sAlphabet_Small(j) Then
+                    iCount += 1
+                End If
+            Next
+            If iCount = 0 Then
+                MessageBox.Show("입력된 문장에 영어 소문자가 아닙니다.", "경고 2")
+                TextBox1.Focus()
+                TextBox1.SelectionStart = i
+                TextBox1.SelectionLength = 1
+                Exit Sub
+            End If
+            iCount = 0
+        Next
+
+        TextBox2.Text = ""
+
+        For i = 0 To iMunjang_Len - 1 ' 대문자로 바꾸고 뒤로 + 3
+            For j = 0 To 25
+                If sMunjang(i) = sAlphabet_Small(j) Then
+                    If j + 3 > 25 Then
+                        TextBox2.Text = TextBox2.Text & sAlphabet_Big((j + 3) - 26) ' xyz 는 한바퀴 돌아서 abc로 
+                    Else
+                        TextBox2.Text = TextBox2.Text & sAlphabet_Big(j + 3)
+                    End If
+                End If
+            Next
+        Next
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If TextBox2.Text = "" Then
+            MessageBox.Show("암호문을 영어 대문자로 입력하세요", "경고 1-1")
+            TextBox2.Focus()
+            Exit Sub
+        End If
+
+        Dim iMunjang_Len As Integer
+        iMunjang_Len = Len(TextBox2.Text)
+        Dim sMunjang(iMunjang_Len) As String
+
+        For i = 0 To iMunjang_Len - 1
+            sMunjang(i) = Mid(TextBox2.Text, i + 1, 1) ' 문장을 한글자씩 잘라서 배열에 저장
+        Next
+
+        Dim iCount As Integer = 0
+
+        For i = 0 To iMunjang_Len - 1
+            For j = 0 To 25
+                If sMunjang(i) = sAlphabet_Big(j) Then
+                    iCount += 1
+                End If
+            Next
+            If iCount = 0 Then
+                MessageBox.Show("입력된 문장에 영어 대문자가 아닙니다.", "경고 2-1")
+                TextBox2.Focus()
+                TextBox2.SelectionStart = i
+                TextBox2.SelectionLength = 1
+                Exit Sub
+            End If
+            iCount = 0
+        Next
+
+        TextBox3.Text = ""
+
+        For i = 0 To iMunjang_Len - 1 ' 소문자로 바꾸고 앞으로 - 3
+            For j = 0 To 25
+                If sMunjang(i) = sAlphabet_Big(j) Then
+                    If j - 3 < 0 Then
+                        TextBox3.Text = TextBox3.Text & sAlphabet_Small((j - 3) + 26) ' abc 는 한바퀴 돌아서 xyz로 
+                    Else
+                        TextBox3.Text = TextBox3.Text & sAlphabet_Small(j - 3)
+                    End If
+                End If
+            Next
+        Next
+    End Sub
+End Class
---
+++ 11주차-암호해석프로그렘(비주얼베이직)/11주차-암호해석프로그램/Form2.Designer.vb
@@ -0,0 +1,176 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class 아스키코드로
+    Inherits System.Windows.Forms.Form
+
+    'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form 디자이너에 필요합니다.
+    Private components As System.ComponentModel.IContainer
+
+    '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
+    '수정하려면 Windows Form 디자이너를 사용하십시오.  
+    '코드 편집기에서는 수정하지 마세요.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("굴림", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Label5.Location = New System.Drawing.Point(1270, 594)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(141, 29)
+        Me.Label5.TabIndex = 21
+        Me.Label5.Text = "<수신측>"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Font = New System.Drawing.Font("굴림", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.TextBox3.Location = New System.Drawing.Point(85, 637)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(1326, 41)
+        Me.TextBox3.TabIndex = 20
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("굴림", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Label6.Location = New System.Drawing.Point(80, 594)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(316, 29)
+        Me.Label6.TabIndex = 19
+        Me.Label6.Text = "다시 평문:영어 소문자"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("굴림", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Label3.Location = New System.Drawing.Point(693, 352)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(107, 116)
+        Me.Label3.TabIndex = 18
+        Me.Label3.Text = "↓" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "↓ 전송" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "↓" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "↓"
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("굴림", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Button2.Location = New System.Drawing.Point(631, 508)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(155, 51)
+        Me.Button2.TabIndex = 17
+        Me.Button2.Text = "복호화"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("굴림", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(85, 277)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(1326, 41)
+        Me.TextBox2.TabIndex = 16
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("굴림", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Label4.Location = New System.Drawing.Point(80, 234)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(245, 29)
+        Me.Label4.TabIndex = 15
+        Me.Label4.Text = "평문:영어 대문자"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("굴림", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Label2.Location = New System.Drawing.Point(1270, 8)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(141, 29)
+        Me.Label2.TabIndex = 14
+        Me.Label2.Text = "<송신측>"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("굴림", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Button1.Location = New System.Drawing.Point(631, 148)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(155, 51)
+        Me.Button1.TabIndex = 13
+        Me.Button1.Text = "암호화"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("굴림", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(85, 51)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(1326, 41)
+        Me.TextBox1.TabIndex = 12
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("굴림", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Label1.Location = New System.Drawing.Point(80, 8)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(245, 29)
+        Me.Label1.TabIndex = 11
+        Me.Label1.Text = "평문:영어 소문자"
+        '
+        '아스키코드로
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1513, 684)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "아스키코드로"
+        Me.Text = "아스키코드로"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+End Class
